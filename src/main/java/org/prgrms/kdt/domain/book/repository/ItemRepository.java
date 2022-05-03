76b7db6d20740674a33384f0ddbc7f66c148e5aa
dbslzld15 <dbslzld15@naver.com>
2022-05-04 00:23:01 +0900

    feat: 아이템 레포지토리 구현

---
+++ src/main/java/org/prgrms/kdt/domain/book/repository/ItemRepository.java
@@ -0,0 +1,18 @@
+package org.prgrms.kdt.domain.book.repository;
+
+import org.prgrms.kdt.domain.book.entity.Item;
+
+import java.util.Optional;
+
+public interface ItemRepository {
+
+    Optional<Item> findById(long itemId);
+
+    long save(Item item);
+
+    long update(Item item);
+
+    void deleteById(long itemId);
+
+    void deleteAll();
+}
---
+++ src/main/java/org/prgrms/kdt/domain/book/repository/JdbcItemRepository.java
@@ -0,0 +1,113 @@
+package org.prgrms.kdt.domain.book.repository;
+
+import org.prgrms.kdt.domain.book.entity.Item;
+import org.prgrms.kdt.domain.book.exception.ItemException;
+import org.prgrms.kdt.domain.book.vo.Price;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.prgrms.kdt.domain.book.exception.ItemExceptionType.ITEM_NOT_SAVED;
+import static org.prgrms.kdt.global.utils.Utils.toLocalDateTime;
+
+@Repository
+public class JdbcItemRepository implements ItemRepository {
+
+    public static final Logger log = LoggerFactory.getLogger(JdbcItemRepository.class);
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public JdbcItemRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public long save(Item item) {
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+        MapSqlParameterSource source = toParameterSource(item);
+        int savedRows = jdbcTemplate.update(
+                "INSERT INTO item(price, stock_quantity, created_at, modified_at) " +
+                        "VALUES (:price, :stockQuantity, :createdAt, :modifiedAt)",
+                source, keyHolder, new String[]{"item_id"}
+        );
+        if (savedRows != 1) {
+            throw new ItemException(ITEM_NOT_SAVED);
+        }
+        return keyHolder.getKey().longValue();
+    }
+
+    @Override
+    public Optional<Item> findById(long itemId) {
+        try {
+            Item item = jdbcTemplate.queryForObject("SELECT * FROM item " +
+                    "WHERE item_id = :itemId", Collections.singletonMap("itemId", itemId), itemRowMapper);
+            return Optional.of(item);
+        } catch (EmptyResultDataAccessException e) {
+            log.error("입력받은 아이디에 해당하는 도서 정보가 존재하지 않습니다", e);
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public long update(Item item) {
+        return jdbcTemplate.update("UPDATE item " +
+                "SET price = :price, stock_quantity = :stockQuantity, modified_at = :modifiedAt " +
+                "WHERE item_id = :itemId", toParamMap(item));
+    }
+
+    @Override
+    public void deleteById(long itemId) {
+        jdbcTemplate.update("DELETE FROM item WHERE item_id = :itemId",
+                Collections.singletonMap("itemId", itemId));
+    }
+
+    @Override
+    public void deleteAll() {
+        jdbcTemplate.update("DELETE FROM item", Collections.emptyMap());
+    }
+
+    private static final RowMapper<Item> itemRowMapper = (rs, i) -> {
+        long itemId = rs.getLong("item_id");
+        long price = rs.getLong("price");
+        int stockQuantity = rs.getInt("stock_quantity");
+        LocalDateTime createdAt = toLocalDateTime(rs.getTimestamp("created_at"));
+        LocalDateTime modifiedAt = toLocalDateTime(rs.getTimestamp("modified_at"));
+
+        return Item.builder()
+                .itemId(itemId)
+                .price(new Price(price))
+                .stockQuantity(stockQuantity)
+                .createdDateTime(createdAt)
+                .modifiedDateTime(modifiedAt)
+                .build();
+    };
+
+    private MapSqlParameterSource toParameterSource(Item item) {
+        MapSqlParameterSource parameters = new MapSqlParameterSource()
+                .addValue("price", item.getPrice().getPrice())
+                .addValue("stockQuantity", item.getStockQuantity())
+                .addValue("createdAt", item.getCreatedDateTime())
+                .addValue("modifiedAt", item.getModifiedDateTime());
+        return parameters;
+    }
+
+    private Map<String, Object> toParamMap(Item item) {
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("itemId", item.getItemId());
+        paramMap.put("price", item.getPrice().getPrice());
+        paramMap.put("stockQuantity", item.getStockQuantity());
+        paramMap.put("createdAt", item.getCreatedDateTime());
+        paramMap.put("modifiedAt", item.getModifiedDateTime());
+        return paramMap;
+    }
+}

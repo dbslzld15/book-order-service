57c48d065291a7de71f99bc341993a01a61177c3
dbslzld15 <dbslzld15@naver.com>
2022-05-04 11:17:48 +0900

    fix: 아이템 서비스 테스트 코드 구현

---
+++ src/test/java/org/prgrms/kdt/domain/book/service/ItemServiceTest.java
@@ -0,0 +1,81 @@
+package org.prgrms.kdt.domain.book.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.prgrms.kdt.domain.book.entity.Item;
+import org.prgrms.kdt.domain.book.repository.ItemRepository;
+import org.prgrms.kdt.domain.book.vo.Price;
+
+import java.util.Optional;
+
+import static java.time.LocalDateTime.now;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class ItemServiceTest {
+
+    @Mock
+    ItemRepository itemRepository;
+
+    @InjectMocks
+    ItemService itemService;
+
+    @Test
+    void save() {
+        //given
+        long itemId = 1L;
+        //when
+        when(itemRepository.save(any())).thenReturn(itemId);
+        long savedId = itemService.save(10000L, 20);
+        //then
+        assertThat(savedId).isEqualTo(itemId);
+    }
+
+    @Test
+    void getItemById() {
+        //given
+        long itemId = 1L;
+        Item item = new Item(now(), now(), itemId, new Price(10000L), 20);
+        //when
+        when(itemRepository.findById(anyLong())).thenReturn(Optional.of(item));
+        Item findItem = itemService.getItemById(itemId);
+        //then
+        assertThat(findItem).usingRecursiveComparison().isEqualTo(item);
+    }
+
+    @Test
+    void update() {
+        //given
+        long itemId = 1L;
+        long price = 1000L;
+        int stockQuantity = 20;
+        Item item = new Item(now(), now(), itemId, new Price(price), stockQuantity);
+        //when
+        when(itemRepository.findById(anyLong())).thenReturn(Optional.of(item));
+        itemService.update(itemId, price, stockQuantity);
+        //then
+        verify(itemRepository, times(1)).findById(anyLong());
+        verify(itemRepository, times(1)).update(any());
+    }
+
+    @Test
+    void remove() {
+        //given
+        long itemId = 1L;
+        long price = 1000L;
+        int stockQuantity = 20;
+        Item item = new Item(now(), now(), itemId, new Price(price), stockQuantity);
+        //when
+        when(itemRepository.findById(anyLong())).thenReturn(Optional.of(item));
+        itemService.remove(itemId);
+        //then
+        verify(itemRepository, times(1)).findById(anyLong());
+        verify(itemRepository, times(1)).deleteById(anyLong());
+    }
+}

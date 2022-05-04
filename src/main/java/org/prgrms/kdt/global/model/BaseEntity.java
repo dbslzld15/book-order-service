5492c230cee3f8f2abc78ded9fc1c073851dc9ae
dbslzld15 <dbslzld15@naver.com>
2022-05-04 23:39:07 +0900

    fix: @Getter 사용하도록 수정

--- src/main/java/org/prgrms/kdt/global/model/BaseEntity.java
+++ src/main/java/org/prgrms/kdt/global/model/BaseEntity.java
@@ -1,7 +1,10 @@
 package org.prgrms.kdt.global.model;
 
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 
+@Getter
 public abstract class BaseEntity {
     private LocalDateTime createdDateTime;
     private LocalDateTime modifiedDateTime;
@@ -9,13 +12,5 @@ public abstract class BaseEntity {
     public BaseEntity(LocalDateTime createdDateTime, LocalDateTime modifiedDateTime) {
         this.createdDateTime = createdDateTime;
         this.modifiedDateTime = modifiedDateTime;
-    }
-
-    public LocalDateTime getCreatedDateTime() {
-        return createdDateTime;
-    }
-
-    public LocalDateTime getModifiedDateTime() {
-        return modifiedDateTime;
     }
 }

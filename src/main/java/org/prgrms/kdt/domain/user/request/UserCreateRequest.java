f838f9ede35e2f931986f7da0a38faa7ffcc495c
dbslzld15 <dbslzld15@naver.com>
2022-05-06 10:17:20 +0900

    fix: 유저 생성 DTO 생성자 추가

--- src/main/java/org/prgrms/kdt/domain/user/request/UserCreateRequest.java
+++ src/main/java/org/prgrms/kdt/domain/user/request/UserCreateRequest.java
@@ -2,16 +2,20 @@ package org.prgrms.kdt.domain.user.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter @Setter
 public class UserCreateRequest {
     @NotBlank
     private String name;
+    @NotBlank
+    private String password;
     @NotBlank
     @Email
     private String email;

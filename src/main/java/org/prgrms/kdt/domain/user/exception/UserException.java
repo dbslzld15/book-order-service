fea74af88348c680fffb60090fa46c2b1957bc16
dbslzld15 <dbslzld15@naver.com>
2022-05-06 21:32:52 +0900

    fix: 유저 예외 처리에서 상태 코드를 갖도록 수정

--- src/main/java/org/prgrms/kdt/domain/user/exception/UserException.java
+++ src/main/java/org/prgrms/kdt/domain/user/exception/UserException.java
@@ -1,7 +1,14 @@
 package org.prgrms.kdt.domain.user.exception;
 
 public class UserException extends RuntimeException {
+    private int statusCode;
+
     public UserException(UserExceptionType userExceptionType) {
         super(userExceptionType.getMsg());
+        this.statusCode = userExceptionType.getStatusCode();
+    }
+
+    public int getStatusCode() {
+        return statusCode;
     }
 }
--- src/main/java/org/prgrms/kdt/domain/user/exception/UserExceptionType.java
+++ src/main/java/org/prgrms/kdt/domain/user/exception/UserExceptionType.java
@@ -1,17 +1,23 @@
 package org.prgrms.kdt.domain.user.exception;
 
 public enum UserExceptionType {
-    USER_NOT_EXIST("존재하지 않는 고객입니다."),
-    USER_PASSWORD_INCORRECT("패스워드가 일치하지 않습니다."),
-    USER_NOT_SAVED("고객 저장에 실패했습니다.");
+    USER_NOT_EXIST("존재하지 않는 고객입니다.", 400),
+    USER_PASSWORD_INCORRECT("패스워드가 일치하지 않습니다.", 400),
+    USER_NOT_SAVED("고객 저장에 실패했습니다.", 500);
 
     private final String msg;
+    private final int statusCode;
 
-    UserExceptionType(String msg) {
+    UserExceptionType(String msg, int statusCode) {
         this.msg = msg;
+        this.statusCode = statusCode;
     }
 
     public String getMsg() {
         return msg;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
     }
 }

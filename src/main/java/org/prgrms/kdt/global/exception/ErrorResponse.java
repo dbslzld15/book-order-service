bd955cb333952cefa19e6f31346e73e055d163d5
dbslzld15 <dbslzld15@naver.com>
2022-05-06 21:32:08 +0900

    feat: controller advice 구현

---
+++ src/main/java/org/prgrms/kdt/global/exception/ErrorResponse.java
@@ -0,0 +1,10 @@
+package org.prgrms.kdt.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ErrorResponse {
+    private String msg;
+}
---
+++ src/main/java/org/prgrms/kdt/global/exception/ExceptionAdvisor.java
@@ -0,0 +1,40 @@
+package org.prgrms.kdt.global.exception;
+
+import org.prgrms.kdt.domain.user.exception.UserException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ExceptionAdvisor{
+
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<ErrorResponse> handleUserException(UserException e) {
+        int statusCode = e.getStatusCode();
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.resolve(statusCode));
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ErrorResponse> handleBindException(BindException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        String msg = bindingResult.getFieldErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.joining(" "));
+        ErrorResponse errorResponse = new ErrorResponse(msg);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleArgumentException(IllegalArgumentException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
+

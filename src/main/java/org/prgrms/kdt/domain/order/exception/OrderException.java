53a98d2da79965d11595ee0a561ce79ad3fc4dcd
dbslzld15 <dbslzld15@naver.com>
2022-05-07 21:43:21 +0900

    fix: 주문 예외 상태코드를 갖도록 수정

--- src/main/java/org/prgrms/kdt/domain/order/exception/OrderException.java
+++ src/main/java/org/prgrms/kdt/domain/order/exception/OrderException.java
@@ -1,7 +1,14 @@
 package org.prgrms.kdt.domain.order.exception;
 
-public class OrderException extends RuntimeException{
+public class OrderException extends RuntimeException {
+    private final int statusCode;
+
     public OrderException(OrderExceptionType exceptionType) {
         super(exceptionType.getMsg());
+        this.statusCode = exceptionType.getStatusCode();
+    }
+
+    public int getStatusCode() {
+        return statusCode;
     }
 }
--- src/main/java/org/prgrms/kdt/domain/order/exception/OrderExceptionType.java
+++ src/main/java/org/prgrms/kdt/domain/order/exception/OrderExceptionType.java
@@ -1,18 +1,25 @@
 package org.prgrms.kdt.domain.order.exception;
 
 public enum OrderExceptionType {
-    ORDER_NOT_EXIST("존재하지 않는 주문입니다."),
-    ORDER_NOT_SAVED("주문 저장에 실패했습니다."),
-    ORDER_CAN_NOT_CANCELED("해당 주문은 취소할 수 없습니다."),
-    ORDER_NOT_ENOUGH_STOCK("남은 재고 수량이 없습니다");
+    ORDER_NOT_EXIST("존재하지 않는 주문입니다.", 400),
+    ORDER_NOT_SAVED("주문 저장에 실패했습니다.", 500),
+    ORDER_CAN_NOT_CANCELED("해당 주문은 취소할 수 없습니다.", 400),
+    ORDER_NOT_ENOUGH_STOCK("남은 재고 수량이 없습니다", 500),
+    ORDER_NOT_STATUS_INCORRECT("지원하지 않는 주문상태 입니다.", 400);
 
     private final String msg;
+    private final int statusCode;
 
-    OrderExceptionType(String msg) {
+    OrderExceptionType(String msg, int statusCode) {
         this.msg = msg;
+        this.statusCode = statusCode;
     }
 
     public String getMsg() {
         return msg;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
     }
 }

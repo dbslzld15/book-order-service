6c8e35829ee1f6735f231283aa6e87558f48b14f
dbslzld15 <dbslzld15@naver.com>
2022-05-07 21:44:27 +0900

    fix: 주문 생성 validation 추가

--- src/main/java/org/prgrms/kdt/domain/order/request/OrderCreateRequest.java
+++ src/main/java/org/prgrms/kdt/domain/order/request/OrderCreateRequest.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @AllArgsConstructor
 @Getter @Setter
 public class OrderCreateRequest {
+    @NotNull(message = "주소는 필수로 입력해야합니다.")
     private String address;
-    private List<OrderItemCreateRequest> orderItemsRequest;
+    private List<OrderItemCreateRequest> orderItems;
 
 }
--- src/main/java/org/prgrms/kdt/domain/order/request/OrderItemCreateRequest.java
+++ src/main/java/org/prgrms/kdt/domain/order/request/OrderItemCreateRequest.java
@@ -4,10 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @AllArgsConstructor
 @Getter @Setter
 public class OrderItemCreateRequest {
+    @NotNull(message = "상품 ID값이 입력되지 않았습니다.")
     private long itemId;
+    @NotNull(message = "총 주문 금액은 필수 입력입니다.")
+    @Positive(message = "총 주문 금액은 음수가 될 수 없습니다.")
     private long totalPrice;
+    @NotNull(message = "주문 수량은 필수 입력입니다.")
+    @Positive(message = "주문 수량은 음수가 될 수 없습니다.")
     private int orderQuantity;
 }
